24e89fed3ebb5f6bc146ec5d9aaf810a1c32a17d
Claudiu Belu <cbelu@cloudbasesolutions.com>
2020-02-13 15:23:50 +0000

    WIP: test images: uses nanoserver

--- test/images/image-util.sh
+++ test/images/image-util.sh
@@ -142,7 +142,7 @@ build() {
       # TODO(claudiub): add "build --isolation=hyperv" once GCE introduces Hyper-V support.
       docker --tlsverify --tlscacert "${HOME}/.docker-${os_version}/ca.pem" \
         --tlscert "${HOME}/.docker-${os_version}/cert.pem" --tlskey "${HOME}/.docker-${os_version}/key.pem" \
-        -H "${REMOTE_DOCKER_URL}" build --pull -t "${REGISTRY}/${image_name}:${TAG}-${os_name}-${arch}-${os_version}" \
+        -H "${REMOTE_DOCKER_URL}" build --no-cache --pull -t "${REGISTRY}/${image_name}:${TAG}-${os_name}-${arch}-${os_version}" \
         --build-arg BASEIMAGE="${BASEIMAGE}" -f $dockerfile_name .
     else
       echo "Cannot build the image '${image}' for ${os_arch}. REMOTE_DOCKER_URL_$os_version should be set, containing the URL to a Windows docker daemon."
